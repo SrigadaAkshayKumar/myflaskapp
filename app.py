e1603f1a127ed5038d575a7456ede9554e601960
Akshayak8 <srigadaakshay@gmail.com>
2024-11-19 22:13:18 +0530

    update4

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def handle_disconnect():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get('PORT', 8080))  # Default to 5000 if PORT is not set
     socketio.run(app, host='0.0.0.0', port=port)
